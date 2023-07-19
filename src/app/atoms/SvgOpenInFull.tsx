2174f62584af8ddd529d6331cec976a79809309a
tingtt <ting.taku@gmail.com>
2023-07-20 01:05:34 +0900

    style(slide-control): fix control button color

--- src/app/atoms/SvgOpenInFull.tsx
+++ src/app/atoms/SvgOpenInFull.tsx
@@ -11,7 +11,7 @@ export const SvgOpenInFull = ({
     viewBox='0 0 44 44'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
-    className='fill-black dark:fill-white'
+    className='fill-base-content'
   >
     <g clipPath='url(#clip0_193_427)'>
       <path d='M6.28571 28.2857H0V44H15.7143V37.7143H6.28571V28.2857ZM0 15.7143H6.28571V6.28571H15.7143V0H0V15.7143ZM37.7143 37.7143H28.2857V44H44V28.2857H37.7143V37.7143ZM28.2857 0V6.28571H37.7143V15.7143H44V0H28.2857Z' />
